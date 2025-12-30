c73185c0b3307fda77f242d48b60e526869d9934
sonmu <sonmu.def@gmail.com>
2025-12-31 04:33:32 +0800

    apt install, tmux resource

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
+
+git config --global user.email "sonmu.def@gmail.com"
+git config --global user.name "sonmu"
 mv ~/.bashrc{,.bak}
 cp ./.bashrc ~/.bashrc
 cp ./.tmux.conf ~/.tmux.conf
 source ~/.bashrc
+apt install git tmux vim
+tmux source-file "$HOME/.tmux.conf" 2>/dev/null || true
+
