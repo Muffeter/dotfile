dc6b89452bef97304e5a3da5eb7d42b9ab570ab9
sonmu <sonmu.def@gmail.com>
2026-01-02 17:44:16 +0800

    update ansible

--- install.sh
+++ install.sh
@@ -5,6 +5,13 @@ mv ~/.bashrc{,.bak}
 cp ./.bashrc ~/.bashrc
 cp ./.tmux.conf ~/.tmux.conf
 source ~/.bashrc
-apt install git tmux vim
+apt install git tmux vim python3
+
+# tmux
 tmux source-file "$HOME/.tmux.conf" 2>/dev/null || true
 
+# ansible
+python3 -m pip install ansible venv
+mkdir ~/ansible
+python3 -m venv ~/ansible/env
+gitmy ansible_playbook ~/ansible/ansible_playbook
